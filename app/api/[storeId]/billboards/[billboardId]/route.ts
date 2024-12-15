2453540b162c37d3e88db9c6878a8dc44127f6fe
keshavgarg5130 <keshav.garg.5130@gmail.com>
2024-12-15 16:37:51 +0530

    jj

--- app/api/[storeId]/billboards/[billboardId]/route.ts
+++ app/api/[storeId]/billboards/[billboardId]/route.ts
@@ -3,7 +3,6 @@ import {auth} from "@clerk/nextjs/server";
 import prismadb from "@/lib/prismadb";
 
 export async function GET (
-    req: Request,
     { params }: { params: { billboardId: string}},
 ){
     try {
