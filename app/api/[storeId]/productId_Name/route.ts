65a0bcc74e33292db61a674e3bd4990897e3aaea
keshavgarg5130 <keshav.garg.5130@gmail.com>
2025-03-06 19:21:06 +0530

    productIdandName

---
+++ app/api/[storeId]/productId_Name/route.ts
@@ -0,0 +1,26 @@
+import {NextResponse} from "next/server";
+import prismadb from "@/lib/prismadb";
+
+
+export async function GET (
+    req: Request,
+    {params} : {params:{storeId:string}}
+
+){
+
+    try {
+        const productId = await prismadb.product.findMany({
+            where:{
+                storeId: params.storeId,
+            },
+            select:{
+                id:true,
+                name:true,
+            }
+
+        })
+        return NextResponse.json(productId)
+    }catch(e){console.log("ProductId_GET Error: ]", e);
+        return new NextResponse("internal Error", {status: 500})}
+
+}
