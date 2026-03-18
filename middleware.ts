b5fedbcf6b766c1a0a0b4c45912248f8122aed12
Keshav Garg <keshav.garg.5130@gmail.com>
2026-03-18 21:40:44 +0530

    Add catch-all route to public route matcher

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)',"/api/(.*)/enquiry",])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)',"/api/(.*)/enquiry",'/api/(.*)',])
 
 export default clerkMiddleware(async (auth, request) => {
     if (!isPublicRoute(request)) {
